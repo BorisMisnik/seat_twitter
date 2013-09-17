d7daee2f44d13df16450c4b081198f3261583f23
Boris <misnikb@gmail.com>
2013-09-18 01:43:02 +0300

    server.js

--- models/index.js
+++ models/index.js
@@ -47,13 +47,19 @@ var model = {
 			// _this.visual = 0;
 			_this.noVisual = 0;
 			model.collection.update({
-				type:'visual',name:'v8'
-				// type:'noVisual',name:'v7',
+				type:'noVisual',name:'v7',
 				// type:'noVisual',name:'v8',
 			},{$set: {user: '', share:false }}, function(err, result){
 				if(err) console.log('err')
 				else{
-					console.log(result)
+					model.collection.update({
+						type:'noVisual',name:'v8',
+					},{$set: {user: '', share:false }}, function(err, result){
+						if(err) console.log('err')
+						else{
+							console.log(result)
+						}
+					})
 				}
 			})
 			_this.visual = 2;
